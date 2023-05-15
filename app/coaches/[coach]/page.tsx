d78e34750023ce7bb251a3e340cc13e4882c00a9
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2023-05-15 10:21:57 -0500

    initial commit

---
+++ app/coaches/[coach]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Coach = () => {
+  return (
+    <div>Each Coach will have their own Bio Page</div>
+  )
+}
+
+export default Coach
---
+++ app/coaches/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Coaches = () => {
+  return (
+    <div>This is the coaches' page. It will show a grid of coach profiles pics and name. </div>
+  )
+}
+
+export default Coaches
